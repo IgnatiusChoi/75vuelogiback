58c9048bb341ad2fe8178e27e8c4465799e2ecff
DaeyupJeong <themdkxm200@gmail.com>
2023-05-13 01:45:38 +0900

    부서정보 관리 뒷단

---
+++ src/main/java/kr/co/seoulit/erp/logistic/base/applicationservice/DeptInfoApplicationService.java
@@ -0,0 +1,9 @@
+package kr.co.seoulit.erp.logistic.base.applicationservice;
+
+import kr.co.seoulit.erp.logistic.base.to.DeptInfoTO;
+
+import java.util.ArrayList;
+
+public interface DeptInfoApplicationService {
+    public ArrayList<DeptInfoTO> getDeptInfo();
+}
