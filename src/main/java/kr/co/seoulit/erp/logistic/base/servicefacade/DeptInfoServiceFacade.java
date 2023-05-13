2e3bbdb47a76c42456fa78adb92e2a4c98465513
DaeyupJeong <themdkxm200@gmail.com>
2023-05-13 11:10:10 +0900

    부서정보 수정_ApplicationService 제거

---
+++ src/main/java/kr/co/seoulit/erp/logistic/base/servicefacade/DeptInfoServiceFacade.java
@@ -0,0 +1,10 @@
+package kr.co.seoulit.erp.logistic.base.servicefacade;
+
+import kr.co.seoulit.erp.logistic.base.to.DeptInfoTO;
+
+import java.util.ArrayList;
+
+public interface DeptInfoServiceFacade {
+
+    public ArrayList<DeptInfoTO> getDeptInfo();
+}
