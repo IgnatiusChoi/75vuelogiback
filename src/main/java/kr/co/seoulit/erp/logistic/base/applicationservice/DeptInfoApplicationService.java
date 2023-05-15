62c821b811dfd65ed496281cf6e7459574a22e2b
IgnatiusChoi <128049025+IgnatiusChoi@users.noreply.github.com>
2023-05-15 10:32:30 +0900

    불필요파일제거 (#14)

--- src/main/java/kr/co/seoulit/erp/logistic/base/applicationservice/DeptInfoApplicationService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.seoulit.erp.logistic.base.applicationservice;
-
-import kr.co.seoulit.erp.logistic.base.to.DeptInfoTO;
-
-import java.util.ArrayList;
-
-public interface DeptInfoApplicationService {
-    public ArrayList<DeptInfoTO> getDeptInfo();
-}
